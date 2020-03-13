199d555cbf096ee7385ad38c4d352364f58e582b
Madhumitha-Illavarasu <53329769+Madhumitha-Illavarasu@users.noreply.github.com>
2020-03-13 17:41:16 +0530

    Add files via upload

---
+++ tsInterface/dist.ts
@@ -0,0 +1,16 @@
+interface Ipoint {
+    getDist(): number;
+}
+module Shapes {
+    export class Point implements Ipoint {
+        constructor(public x: number, public y: number) {      }
+        getDist(){
+            return Math.sqrt(this.x*this.x+this.y*this.y);
+        }
+       // static origin=new Point(0,0);
+    }
+
+}
+var p:Ipoint=new Shapes.Point(3,4);
+var dist=p.getDist();
+console.log(dist);
